477788d589c332c4bff56791270bbe38ce1a6715
leloc0609 <80434238+leloc0609@users.noreply.github.com>
2022-05-30 21:59:29 +0700

    relay update

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 from tkinter import *
 from PIL import Image, ImageTk
 WELCOME = 'PRESS LOGO TO CONTINUE'
@@ -6,6 +7,7 @@ class Fullscreen_Window:
         self.tk = Tk()
         self.tk.config(background="white")
         self.state = True
+        self.tk.attributes("-fullscreen", self.state)
         self.tk.bind("<F11>", self.toggle_fullscreen)
 
     def toggle_fullscreen(self, event=None):
@@ -24,30 +26,33 @@ class Init_Screen:
         self.createWidgets()
 
     def createWidgets(self):
-
-        self.logoFrame = Frame(self.master, background="white")
-        self.logoFrame.place(in_=self.master, anchor="c", relx=.5, rely=.5)
-        self.logo = ImageTk.PhotoImage(Image.open("resource/BKINNO_logo.png"))
-        self.logoButton = Button(self.logoFrame, image=self.logo, border=0, borderwidth=0)
-        self.logoButton.grid(column=0, row=0, pady=10)
-        self.label = Label(self.logoFrame, text=self.labelText, background="white", font= ('Helvatica', 30, 'bold'))
+        self.logoInitFrame = Frame(self.master, background="white")
+        self.logoInitFrame.place(in_=self.master, anchor="c", relx=.5, rely=.5)
+        self.logo = ImageTk.PhotoImage(Image.open("D:\BK INNP\code\BKINNO_logo.png"))
+        self.logoInitButton = Button(self.logoInitFrame, image=self.logo, border=0, borderwidth=0, command=self.initButtonAction)
+        self.logoInitButton.grid(column=0, row=0, pady=10)
+        self.label = Label(self.logoInitFrame, text=self.labelText, background="white", font= ('Helvatica', 30, 'bold'))
         self.label.grid(column=0, row=1, pady=10)
+        self.label.after(self.interval, self.updateInitialLabel)
 
-        self.label.after(self.interval, self.updateLabel)
-    def updateLabel(self):
+    def updateInitialLabel(self):
         self.labelText= self.labelText + WELCOME[self.index]
         self.label.config(text=self.labelText)
-
         self.index += 1
         if self.index==len(WELCOME):
             self.index=0
             self.labelText=''
 
-        self.label.after(self.interval, self.updateLabel)
+        self.label.after(self.interval, self.updateInitialLabel)
 
+    def initButtonAction(self):
+        self.logoInitFrame.destroy()
+        self.logoInitFrame = Frame(self.master, background="white")
+        self.logoInitFrame.place(in_=self.master, anchor="c", relx=.5, rely=.5)
+        self.logo = ImageTk.PhotoImage(Image.open("D:\BK INNP\code\BKINNO_logo.png"))
+        self.logoInitButton = Button(self.logoInitFrame, image=self.logo, border=0, borderwidth=0)
 
 if __name__ == '__main__':
     w = Fullscreen_Window()
     initScreen = Init_Screen(w.tk)
-
     w.tk.mainloop()
---
+++ testRelay.py
@@ -0,0 +1,48 @@
+# RELAY
+# - Lấy trạng thái
+# Request:  ID 03 00 00 00 01 CRCL CRCH
+# Response: ID 03 02 00 xx CRCL CRCH (xx: 0xFF: ON, 0x00: OFF)
+# - Gắn trạng thái
+# Request:  ID 06 00 00 00 xx CRCL CRCH (xx: 0xFF: ON, 0x00: OFF)
+# Response: ID 06 00 00 00 xx CRCL CRCH
+
+import time
+from serial import Serial
+from modbus485 import *
+
+def serial_read_data(serial, length):
+    out = []
+    time.sleep(0.5)
+
+    #read number of bytes in buffer
+    byteToRead = serial.inWaiting()
+
+    #read array of bytes from buffer
+    if byteToRead > 0:
+        out = serial.read(byteToRead)
+
+    #convert data from byte to int
+    data_out = [b for b in out]
+
+    return data_out
+
+def test_relay_485(serial):
+    relay_on = [0x01, 0x06, 0x00, 0x00, 0x00, 0xFF] #0x01 dau tien la id cai relay
+
+    relay_off = [0x01, 0x06, 0x00, 0x00, 0x00, 0x00]
+    print("ON")
+    serial.write(addCRC16(relay_on))
+    result = serial_read_data(serial, 100)
+    print(result)
+    time.sleep(2)
+    print("OFF")
+    serial.write(addCRC16(relay_off))
+    result = serial_read_data(serial, 100)
+    print(result)
+    time.sleep(2)
+
+serialCommunication = Serial(port="COM11", baudrate=9600)
+
+while True:
+    test_relay_485(serialCommunication)
+    time.sleep(5) 
